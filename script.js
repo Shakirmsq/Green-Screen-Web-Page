c8cc2d7a61bb8fb94d83bfe7ab987371a8e06efb
Mohammed Shakir <shakirmsq@gmail.com>
2023-01-19 01:27:58 +0530

    Second Commit, Convert to Grayscale

--- script.js
+++ script.js
@@ -7,16 +7,40 @@
 
 // Also can be done by another technique
 
-function upload() {
-    var fileinput = document.getElementById("finput");
 
+// Global variable
+
+var image;
+var grayimage;
+
+function upload() {
+
+
+    var fileinput = document.getElementById("finput");
     // create SimpleImage from chosen file
-    var image = new SimpleImage(fileinput);
+    image = new SimpleImage(fileinput);
+    grayimage = new SimpleImage(fileinput);
 
     // Get canvas
     var canvas = document.getElementById("can");
+    var canvas2 = document.getElementById("can2");
 
     // Draw image on canvas
     image.drawTo(canvas);
+    grayimage.drawTo(canvas2);
+
+}
+
+function makeGray() {
+
+    for (var px of image.values()) {
+
+        var avg = px.getRed() + px.getGreen() + px.getBlue();
+        px.setRed(avg);
+        px.setGreen(avg);
+        px.setBlue(avg);
+    }
+    var canvas2 = document.getElementById("can2");
+    image.drawTo(canvas2);
 
 }
